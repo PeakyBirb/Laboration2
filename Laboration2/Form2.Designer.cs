2d27cbaaa3716521570388faf1588159f53799f9
Phyrexiian <s182240@student.hb.se>
2020-01-10 18:36:19 +0100

    .

--- Laboration2/Form2.Designer.cs
+++ Laboration2/Form2.Designer.cs
@@ -28,19 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.VäljStudenterText = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // VäljStudenterText
+            // 
+            this.VäljStudenterText.AutoSize = true;
+            this.VäljStudenterText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VäljStudenterText.Location = new System.Drawing.Point(12, 48);
+            this.VäljStudenterText.Name = "VäljStudenterText";
+            this.VäljStudenterText.Size = new System.Drawing.Size(297, 20);
+            this.VäljStudenterText.TabIndex = 0;
+            this.VäljStudenterText.Text = "Välj vilka studenter som ska ingå i kursen";
+            this.VäljStudenterText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(204, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(59, 182);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(753, 412);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.VäljStudenterText);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label VäljStudenterText;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
